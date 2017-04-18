cf2ec66c998d9f9264bd4641192b2142a3d963ee
hulong <hulong@yiqihao.com>
2017-04-18 14:18:34 +0800

    添加drawerlayout

--- app/src/main/java/com/xtec/timeline/ui/fragment/MoreFragment.java
+++ app/src/main/java/com/xtec/timeline/ui/fragment/MoreFragment.java
@@ -3,9 +3,14 @@ package com.xtec.timeline.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.xtec.timeline.R;
@@ -25,6 +30,18 @@ public class MoreFragment extends Fragment {
     private static final String TAG = "MoreFragment";
     @BindView(R.id.tv_more)
     TextView tvMore;
+    @BindView(R.id.topbar_left)
+    ImageButton topbarLeft;
+    @BindView(R.id.topbar_title)
+    TextView topbarTitle;
+    @BindView(R.id.topbar_right)
+    ImageButton topbarRight;
+    @BindView(R.id.fl_content)
+    FrameLayout flContent;
+    @BindView(R.id.ll_drawer)
+    LinearLayout llDrawer;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
     private View view;
 
 
@@ -43,11 +60,30 @@ public class MoreFragment extends Fragment {
         topBar.setBackgroundColor(getResources().getColor(R.color.blue));
 
         ButterKnife.bind(this, view);
+
+        topbarRight.setImageResource(R.drawable.ic_star);
         return view;
     }
 
-    @OnClick(R.id.tv_more)
-    public void onClick() {
-        UpdateManager.checkNewVersion(getActivity(),false);
+    @OnClick({R.id.tv_more,R.id.topbar_right})
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_more:
+                UpdateManager.checkNewVersion(getActivity(), false);
+                drawerLayout.closeDrawer(Gravity.RIGHT);
+                break;
+            case R.id.topbar_right:
+                if(drawerLayout.isDrawerOpen(llDrawer)){
+                    drawerLayout.closeDrawer(Gravity.RIGHT);
+                }else{
+                    drawerLayout.openDrawer(Gravity.RIGHT);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
     }
 }
