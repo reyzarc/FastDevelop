f703957489a7ccfb89448b630dc51859ad3b3bc9
hulong <hulong@yiqihao.com>
2017-07-12 15:31:28 +0800

    终发放端测

--- app/src/main/java/com/xtec/timeline/widget/calendarlayout/MeizuCalendarView.java
+++ app/src/main/java/com/xtec/timeline/widget/calendarlayout/MeizuCalendarView.java
@@ -4,7 +4,10 @@ package com.xtec.timeline.widget.calendarlayout;
  * author : 武昌丶鱼
  * e-mail : reyzarc@163.com
  * time   : 2017-07-11
- * desc
+ * desc     仿魅族日历
  */
-public class MeizuCalendarVIew {
+public class MeizuCalendarView {
+    private static final String TAG = "MeizuCalendarVIew";
+    
+    
 }
